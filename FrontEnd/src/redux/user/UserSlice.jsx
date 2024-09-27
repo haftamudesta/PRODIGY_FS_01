0576f7c8edbec7fef650bb798ec6ddc235e1b8bb
haftamudesta <desta451616@hotmail.com>
2024-09-27 04:00:25 -0800

    Add initial configuration

---
+++ FrontEnd/src/redux/user/UserSlice.jsx
@@ -0,0 +1,74 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    currentUser:null,
+    error:null,
+    loading:false,
+}
+
+const userSlice=createSlice({
+   name:"user",
+   initialState,
+   reducers:{
+    signInStart:(state)=>{
+        state.loading=true,
+        state.error=null
+    },
+    signInSuccess:(state,action)=>{
+        state.currentUser=action.payload,
+        state.loading=false,
+        state.error=null
+    },
+    signInFailure:(state,action)=>{
+        state.error=action.payload,
+        state.loading=false
+    },
+    updateStart:(state)=>{
+        state.loading=true,
+        state.error=null
+    },
+    updateSuccess:(state,action)=>{
+        state.loading=false,
+        state.error=null,
+        state.currentUser=action.payload
+    },
+    updateFailure:(state,action)=>{
+        state.error=action.payload,
+        state.loading=false 
+    },
+    deleteStart:(state)=>{
+        state.loading=true,
+        state.error=null
+    },
+    deleteSuccess:(state)=>{
+        state.loading=false,
+        state.error=null,
+        state.currentUser=null
+    },
+    deleteFailure:(state,action)=>{
+        state.error=action.payload,
+        state.loading=false 
+    },
+    signoutSuccess:(state)=>{
+        state.loading=false,
+        state.error=null,
+        state.currentUser=null
+    },
+   },
+});
+
+export const 
+{
+signInStart,
+signInSuccess,
+signInFailure,
+updateStart,
+updateSuccess,
+updateFailure,
+deleteStart,
+deleteSuccess,
+deleteFailure,
+signoutSuccess
+}
+=userSlice.actions;
+export default userSlice.reducer;
